113b2c8a37cf335dddb91ea84227181122ec7b48
SOW LI WANG <152949802+Brandon08Sow@users.noreply.github.com>
2025-06-17 16:20:35 +0800

    config

--- lib/myconfig.dart
+++ lib/myconfig.dart
@@ -1,5 +1,5 @@
 class MyConfig {
-  static const String server = "http://10.19.24.197/lab_assignment2";
+  static const String server = "http://10.19.119.172/lab_assignment2";
   static const int minPasswordLength = 6;
   static const String appName = "WTMS";
 }
